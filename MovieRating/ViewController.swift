549ff498dfaad66100761126a85a02a72feaff86
cagatay-ogut <ogutcagatay@gmail.com>
2019-05-11 17:47:18 +0300

    Initial Commit

---
+++ MovieRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieRating
+//
+//  Created by cagatay_personal on 11.05.19.
+//  Copyright © 2019 cagatay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
